c75b1b3ee61f957cd49c189cc391cbff0ac905e8
CloudBerryHue <Gitrandomemail@email.com>
2024-09-15 20:30:21 +0200

    added count words function

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    book_word = count_the_words(text)
+    #print(text)
+    print(book_word)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_the_words(book):
+    words = book.split()
+    return len(words)
 
 main()
