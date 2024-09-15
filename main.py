2db870d12c1da679261062cfe4319191bde4750d
CloudBerryHue <Gitrandomemail@email.com>
2024-09-15 22:33:08 +0200

    added counting characters

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     book_word = count_the_words(text)
+    character_count = count_the_characters(text)
     #print(text)
-    print(book_word)
+    print(character_count)
 
 
 def get_book_text(path):
@@ -13,5 +14,16 @@ def get_book_text(path):
 def count_the_words(book):
     words = book.split()
     return len(words)
+
+def count_the_characters(characters):
+    lower_character = characters.lower()
+    character_dict = {}
+    for i in lower_character:
+        if i in character_dict:
+            character_dict[i] += 1
+        else:
+            character_dict[i] = 1
+    return character_dict
+
 
 main()
