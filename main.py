de81294919da6e94a0c148f27450c4177e3153d6
CloudBerryHue <Gitrandomemail@email.com>
2024-09-17 22:45:38 +0200

    added report print in main

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ def main():
     text = get_book_text(book_path)
     book_word = count_the_words(text)
     character_count = count_the_characters(text)
-    #print(text)
-    print(character_count)
-
+    #print(character_count)
+    book_dict = character_count.copy()
+    dict_report = dict_list(book_dict)
+    sort_key = sort_on(book_dict)
+    dict_report.sort(key=sort_on, reverse=True)
+    filtered_list = []
+    for dict_item in dict_report:
+        for char, count in dict_item.items():
+            if char.isalpha():
+                filtered_list += dict_item.items()
+    
+    print(f"--- Begin report of {book_path} --- \n{book_word} words found in the document")
+    for x ,z in filtered_list:
+        print(f"The '{x}' character was found {z} times")
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -24,6 +36,14 @@ def count_the_characters(characters):
         else:
             character_dict[i] = 1
     return character_dict
+
+def dict_list(char_dict):
+    report_list = [{k: v} for k, v in char_dict.items()]
+    return report_list
+
+def sort_on(item):
+    return list(item.values())[0]
+
 
 
 main()
